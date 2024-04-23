dc3f364ca2f315b5af2d70fcfb65ab9c8eea0c8e
IbanezHuang <hf.ibanez@gmail.com>
2024-04-23 13:44:13 +0800

    feat: modify nameing

--- src/app/pages/my-blog/my-blog.routes.ts
+++ src/app/pages/my-blog/my-blog.routes.ts
@@ -5,10 +5,11 @@ import { ArticleComponent } from './article/article.component';
 export const MY_BLOG_ROUTES: Routes = [
   {
     path: 'MyBlog',
+    data: { breadcrumb: '博客管理' },
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'Index' },
-      { path: 'Index', component: IndexComponent },
-      { path: 'Article', component: ArticleComponent },
+      { path: 'Index', component: IndexComponent, data: { breadcrumb: '博客首頁' } },
+      { path: 'Article', component: ArticleComponent, data: { breadcrumb: '文章管理' } },
     ]
   }
 ];
