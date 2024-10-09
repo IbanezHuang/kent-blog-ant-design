4d9c057f0a659414f1b2d7bfacd4fcfd66f6a0e5
IbanezHuang <hf.ibanez@gmail.com>
2024-10-09 22:01:28 +0800

    update index

--- src/app/pages/my-blog/index/index.component.ts
+++ src/app/pages/my-blog/index/index.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [NzCardModule],
+  imports: [NzCardModule, NzFlexModule ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
