e9923be22d1cde6de7377d4dbc59a97ea1e6161c
IbanezHuang <hf.ibanez@gmail.com>
2025-03-23 21:49:11 +0800

    feat: add pdf viewer

--- src/app/pages/my-blog/article/article.component.ts
+++ src/app/pages/my-blog/article/article.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+// import { pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [NgxExtendedPdfViewerModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+
+  constructor() {
+    // pdfDefaultOptions.assetsFolder = 'bleeding-edge';
+  }
+  // pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  public pdfSrc = '../../../assets/pdf/pdf-test.pdf'
+
 
 }
